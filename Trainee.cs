b6066081b2116523755c0cb28d15595627841fde
rahulkhartude <90925722+rahulkhartude@users.noreply.github.com>
2022-07-19 09:53:29 +0530

    Add files via upload

---
+++ Trainee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLib
+{
+   public  class Trainee:Employee
+    {
+        int noOfDays;
+        double ratePerDays;
+        public int NoOfDays 
+        {
+            get { return noOfDays; }
+            set { noOfDays = value; }   
+        }
+        public double RatePerDays   
+        {
+            get { return ratePerDays; } 
+            set { ratePerDays = value; } 
+        }
+        public Trainee ()
+        {
+
+        }
+        public Trainee(string name,string address,int noOfDays,double ratePerDays):base(name,address)
+        {
+            this.noOfDays = noOfDays;   
+            this.ratePerDays = ratePerDays; 
+        }
+        public override double CalculateSalary()
+        {
+            return noOfDays * ratePerDays;  
+        }
+    }
+}
